b621ab626682f7b60d632cc0a7ab1224fec018ea
chanakaroshane <chanaka.778328332@gmail.com>
2026-02-13 21:35:01 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Chanaka</h1>
+      <h1>Vite + React + Buddhika</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
